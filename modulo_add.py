ef94dc943f17fb0d15548525e983971766f822ca
Yan Neves <yanvianaaa@gmail.com>
2026-03-31 16:25:44 -0300

    Validação de duplicidade
    Verifica se os novos dados a serem adicionados ja estão presentes no bd
    Se sim, return False e não adiciona.
    Se não, return True e adiciona.
    Correção de pequenos erros de sintaxe em algumas linhas do codigo.

--- modulo_add.py
+++ modulo_add.py
@@ -1,15 +1,24 @@
 import sqlite3
 
 def adicionar_materia(cursor, nome):
-    cursor.execute("INSERT INTO materias (m_nome) VALUES (?)", (nome,))
+    cursor.execute("SELECT m_id FROM materias WHERE m_nome = (?)", (nome, ))
+    if cursor.fetchone():
+        return False
+    else:
+        cursor.execute("INSERT INTO materias (m_nome) VALUES (?)", (nome,))
+        return True
 
 def adicionar_assunto(cursor, m_nome, nome):
     cursor.execute("SELECT m_id FROM materias WHERE m_nome = (?)", (m_nome,))
     m_id0 = cursor.fetchone()
     if m_id0:
         m_id = m_id0[0]
-        cursor.execute("INSERT INTO assuntos (m_id, a_nome) VALUES (?, ?)", (m_id, nome))
-        return True
+        cursor.execute("SELECT m_id FROM assuntos WHERE m_id = (?) AND a_nome = (?)", (m_id, nome))
+        if cursor.fetchone():
+            return False
+        else:
+            cursor.execute("INSERT INTO assuntos (m_id, a_nome) VALUES (?, ?)", (m_id, nome))
+            return True
     else:
         return False
 
@@ -18,8 +27,12 @@ def adicionar_topico(cursor, a_nome, nome):
     a_id0 = cursor.fetchone()
     if a_id0:
         a_id = a_id0[0]
-        cursor.execute("INSERT INTO topicos (a_id, t_nome) VALUES (?, ?)", (a_id, nome))
-        return True
+        cursor.execute("SELECT a_id FROM topicos WHERE a_id = (?) AND t_nome = (?)", (a_id, nome))
+        if cursor.fetchone():
+            return False
+        else:
+            cursor.execute("INSERT INTO topicos (a_id, t_nome) VALUES (?, ?)", (a_id, nome))
+            return True
     else:
         return False
 
@@ -28,7 +41,11 @@ def adicionar_subtopico(cursor, t_nome, nome, data_estudo, ultima_revisao, numer
     t_id0 = cursor.fetchone()
     if t_id0:
         t_id = t_id0[0]
-        cursor.execute("INSERT INTO subtopico (t_id, s_nome, data_estudo, ultima_revisao, numero_revisoes, data_revisar) VALUES (?, ?, ?, ?, ?, ?)", (t_id, nome, data_estudo, ultima_revisao, numero_revisoes, data_revisar))
-        return True
+        cursor.execute("SELECT t_id FROM subtopico WHERE t_id = (?) AND s_nome = (?)", (t_id, nome))
+        if cursor.fetchone():
+            return False
+        else:
+            cursor.execute("INSERT INTO subtopico (t_id, s_nome, data_estudo, ultima_revisao, numero_revisoes, data_revisar) VALUES (?, ?, ?, ?, ?, ?)", (t_id, nome, data_estudo, ultima_revisao, numero_revisoes, data_revisar))
+            return True
     else:
         return False
