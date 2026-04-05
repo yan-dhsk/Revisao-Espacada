a57aeb7e3de868028e79b9547a467fd81d30a9a6
Yan Neves <yanvianaaa@gmail.com>
2026-04-04 21:10:09 -0300

    Ativação do PRAGMA para foreign keys
    - Ativação do pragma para foreign keys.
    - Permite ao sql trabalhar com foreign keys
    - Inserção de CASCADE nas tabelas filhas para apagação automatica das linhas
    - Importação do modulo_deletar

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sqlite3
 import modulo_add
 import modulo_update
+import modulo_delete
 
 estudo = sqlite3.connect("Revisões.db")
 cursor = estudo.cursor()
 
+cursor.execute("PRAGMA foreign_keys = ON")
+
 cursor.execute("CREATE TABLE IF NOT EXISTS materias (m_id INTEGER PRIMARY KEY AUTOINCREMENT,  m_nome TEXT)")
-cursor.execute("CREATE TABLE IF NOT EXISTS assuntos (a_id INTEGER PRIMARY KEY AUTOINCREMENT,  m_id INTEGER, a_nome TEXT, FOREIGN KEY (m_id) REFERENCES materias(m_id))")
-cursor.execute("CREATE TABLE IF NOT EXISTS topicos (t_id INTEGER PRIMARY KEY AUTOINCREMENT, a_id INTEGER, t_nome TEXT, FOREIGN KEY (a_id) REFERENCES assuntos(a_id))")
-cursor.execute("CREATE TABLE IF NOT EXISTS subtopico (s_id INTEGER PRIMARY KEY AUTOINCREMENT, t_id INTEGER, s_nome TEXT, data_estudo DATE, ultima_revisao DATE, numero_revisoes INTEGER, data_revisar DATE, FOREIGN KEY (t_id) REFERENCES topicos(t_id))")
+cursor.execute("CREATE TABLE IF NOT EXISTS assuntos (a_id INTEGER PRIMARY KEY AUTOINCREMENT,  m_id INTEGER, a_nome TEXT, FOREIGN KEY (m_id) REFERENCES materias(m_id)) ON DELETE CASCADE")
+cursor.execute("CREATE TABLE IF NOT EXISTS topicos (t_id INTEGER PRIMARY KEY AUTOINCREMENT, a_id INTEGER, t_nome TEXT, FOREIGN KEY (a_id) REFERENCES assuntos(a_id)) ON DELETE CASCADE")
+cursor.execute("CREATE TABLE IF NOT EXISTS subtopico (s_id INTEGER PRIMARY KEY AUTOINCREMENT, t_id INTEGER, s_nome TEXT, data_estudo DATE, ultima_revisao DATE, numero_revisoes INTEGER, data_revisar DATE, FOREIGN KEY (t_id) REFERENCES topicos(t_id)) ON DELETE CASCADE")
 
 estudo.commit()
