8a61cea8d689c70a9e035c422ae64eca089d47c8
Yan Neves <yanvianaaa@gmail.com>
2026-04-08 17:40:08 -0300

    Correção de erros e implementação parcial
    - Correção de erros de logica com as query do SQL
    - Separação em funções para melhor manutenção e funcionamento
    - Correção de erros 'fora de index'
    - Implementação das labels com as materias, assuntos, topicos e subtopicos que devem ser revisados no dia.

--- modulo_interface_telaPrincipal.py
+++ modulo_interface_telaPrincipal.py
@@ -8,30 +8,62 @@ cursor = bancoDados.cursor()
 largura = 800
 altura = 600
 
-root = tk.Tk()
-root.title("Revisões espaçadas")
-root.geometry(f"{largura}x{altura}")
+def tela_principal():
+    root = tk.Tk()
+    root.title("Revisões espaçadas")
+    root.geometry(f"{largura}x{altura}")
 
-inicio = tk.Label(root, text="Bem-Vindo", font=("Arial", 28))
-inicio.pack()
+    inicio = tk.Label(root, text="Bem-Vindo", font=("Arial", 28))
+    inicio.pack()
 
-hoje = tk.Label(root, text="Revisar hoje:", font=("Arial", 14))
-hoje.place(x= (largura*0.025), y=(altura*0.125))
+    hoje = tk.Label(root, text="Revisar hoje:", font=("Arial", 14))
+    hoje.place(x= (largura*0.025), y=(altura*0.125))
 
-cursor.execute("SELECT * FROM subtopico WHERE data_revisar = (?)", (date.today(), ))
-revisoes_hoje = cursor.fetchone()
+    desenhar_revisoes_hoje(root)
 
-espaço = 30
-indice = 0
+    root.mainloop()
 
-if revisoes_hoje:
-    revisao = tk.Label(root, text=revisoes_hoje[indice], font=("Arial", 10))
-    revisao.place(x= (largura*0.025), y=((altura+espaço)*0.125))
+def desenhar_revisoes_hoje(root):
+    
+    cursor.execute("SELECT s_nome, t_id FROM subtopico WHERE data_revisar = (?)", (date.today(), ))
+    
+    subtopicos = cursor.fetchall()
+    topicos = []
+    assuntos = []
+    materias = []
 
-if revisoes_hoje != None and indice < len(revisoes_hoje):
-    espaço += 10
-    indice += 1
+    for x in range (len(subtopicos)):
+        cursor.execute("SELECT t_nome, a_id FROM topicos WHERE t_id = (?)", (subtopicos[x][1], ))
+        topicos.append(cursor.fetchall())
 
-root.mainloop()
+    for x in range (len(topicos)):
+        cursor.execute("SELECT a_nome, m_id FROM assuntos WHERE a_id = (?)", (topicos[x][0][1], ))
+        assuntos.append(cursor.fetchall())
 
+    for x in range (len(assuntos)):
+        cursor.execute("SELECT m_nome FROM materias WHERE m_id = (?)", (assuntos[x][0][1], ))
+        materias.append(cursor.fetchall())
 
+    espaço = 300
+    indice = 0
+
+    while True:
+        if subtopicos != None and indice < len(subtopicos):
+            revisao3 = tk.Label(root, text=materias[indice][0], font=("Arial", 10))
+            revisao3.place(x= ((largura + 400)*0.025), y=((altura + espaço)*0.125))
+
+            revisao2 = tk.Label(root, text=assuntos[indice][0][0], font=("Arial", 10))
+            revisao2.place(x= ((largura + 5900)*0.025), y=((altura + espaço)*0.125))
+            
+            revisao1 = tk.Label(root, text=topicos[indice][0][0], font=("Arial", 10))
+            revisao1.place(x= ((largura + 11400)*0.025), y=((altura + espaço)*0.125))
+
+            revisao = tk.Label(root, text=subtopicos[indice][0], font=("Arial", 10))
+            revisao.place(x= ((largura + 16900)*0.025), y=((altura + espaço)*0.125))
+
+            espaço += 200
+            indice += 1
+        else:
+            break;
+
+tela_principal()
