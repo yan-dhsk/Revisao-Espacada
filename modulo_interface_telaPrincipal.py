9d64a49fbead0e5407dc144eaab63f006c267a81
Yan Neves <yanvianaaa@gmail.com>
2026-04-09 19:08:09 -0300

    Nova seção: revisar depois
    - Implementação de uma nova seção na interface
    - Verifica se o resultado da query tem a coluna data_revisar > date.today()
    - Se sim, coloca o subtopico na seção 'revisar depois' e apresenta como uma Label para o usuario

--- modulo_interface_telaPrincipal.py
+++ modulo_interface_telaPrincipal.py
@@ -17,11 +17,59 @@ def tela_principal():
     inicio.pack()
 
     hoje = tk.Label(root, text="Revisar hoje:", font=("Arial", 14))
-    hoje.place(x= (largura*0.025), y=(altura*0.125))
+    hoje.place(x= (largura*0.025), y=((altura)*0.125))
 
-    desenhar_revisoes_hoje(root)
+    medida_base = desenhar_revisoes_hoje(root) + 200
+    desenhar_revisar_depois(root, medida_base)
 
     root.mainloop()
+
+def desenhar_revisar_depois(root, medida_base):
+    cursor.execute("SELECT s_nome, t_id FROM subtopico WHERE data_revisar > (?)", (date.today(), ))
+
+    revisar_depois = tk.Label(root, text="Revisar depois:", font=('Arial', 14))
+    revisar_depois.place(x = (largura*0.025), y=(medida_base*0.125))
+
+    subtopicos = cursor.fetchall()
+    topicos = []
+    assuntos = []
+    materias = []
+
+    for x in range (len(subtopicos)):
+        cursor.execute("SELECT t_nome, a_id FROM topicos WHERE t_id = (?)", (subtopicos[x][1], ))
+        topicos.append(cursor.fetchall())
+
+    for x in range (len(topicos)):
+        cursor.execute("SELECT a_nome, m_id FROM assuntos WHERE a_id = (?)", (topicos[x][0][1], ))
+        assuntos.append(cursor.fetchall())
+
+    for x in range (len(assuntos)):
+        cursor.execute("SELECT m_nome FROM materias WHERE m_id = (?)", (assuntos[x][0][1], ))
+        materias.append(cursor.fetchall())
+
+    espaço = 400
+    indice = 0
+
+    desenhar_nomes_colunas(root, largura, medida_base)
+
+    while True:
+        if subtopicos != None and indice < len(subtopicos):
+            revisao3 = tk.Label(root, text=materias[indice][0], font=("Arial", 10))
+            revisao3.place(x= ((largura + 400)*0.025), y=((medida_base + espaço)*0.125))
+
+            revisao2 = tk.Label(root, text=assuntos[indice][0][0], font=("Arial", 10))
+            revisao2.place(x= ((largura + 5900)*0.025), y=((medida_base + espaço)*0.125))
+            
+            revisao1 = tk.Label(root, text=topicos[indice][0][0], font=("Arial", 10))
+            revisao1.place(x= ((largura + 11400)*0.025), y=((medida_base + espaço)*0.125))
+
+            revisao = tk.Label(root, text=subtopicos[indice][0], font=("Arial", 10))
+            revisao.place(x= ((largura + 16900)*0.025), y=((medida_base + espaço)*0.125))
+
+            espaço += 200
+            indice += 1
+        else:
+            break;
 
 def desenhar_revisoes_hoje(root):
     
@@ -44,8 +92,10 @@ def desenhar_revisoes_hoje(root):
         cursor.execute("SELECT m_nome FROM materias WHERE m_id = (?)", (assuntos[x][0][1], ))
         materias.append(cursor.fetchall())
 
-    espaço = 300
+    espaço = 400
     indice = 0
+
+    desenhar_nomes_colunas(root, largura, altura)
 
     while True:
         if subtopicos != None and indice < len(subtopicos):
@@ -65,5 +115,24 @@ def desenhar_revisoes_hoje(root):
             indice += 1
         else:
             break;
+
+    return espaço + altura
+
+def desenhar_nomes_colunas(root, largura, altura):
+    materias_coluna = tk.Label(root, text="Materias", font=('Arial', 12))
+    materias_coluna.place(x= ((largura + 400)*0.025), y=((altura+200)*0.125))
+
+    assuntos_coluna = tk.Label(root, text="Assuntos", font=('Arial', 12))
+    assuntos_coluna.place(x= ((largura + 5900)*0.025), y=((altura+200)*0.125))
+
+    topicos_coluna = tk.Label(root, text="Tópicos", font=('Arial', 12))
+    topicos_coluna.place(x= ((largura + 11400)*0.025), y=((altura+200)*0.125))   
+
+    subtopicos_coluna = tk.Label(root, text="Subtópicos", font=('Arial', 12))
+    subtopicos_coluna.place(x= ((largura + 16900)*0.025), y=((altura+200)*0.125))  
+
+    linha = tk.Canvas(root, width=560, height=2, bg="#000000")
+    linha.place(x= ((largura + 400)*0.025), y=((altura+360)*0.125))
+    linha.create_line(0, 0, 700, 0, fill="#000000", width=2)
 
 tela_principal()
