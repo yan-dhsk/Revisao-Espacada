24e83773126ef04c479ae5a417d6eb25d20c397c
Yan Neves <yanvianaaa@gmail.com>
2026-04-12 19:36:36 -0300

    Seção de revisões atrasadas e botão check
    - Nova seção adicionada na interface, revisões atrasadas, reune as materias que deveriam ter sido revisadas anteriormente
    - Botão de check para o usuario marcar quando revisar um assunto
    - O botão altera no bd mas ainda precisa de ajustes para fazer alterações visuais na interface do usuario

--- modulo_interface_telaPrincipal.py
+++ modulo_interface_telaPrincipal.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import sqlite3
 from datetime import date
+import modulo_update
 
 bancoDados = sqlite3.connect("Revisões.db")
 cursor = bancoDados.cursor()
@@ -17,23 +18,37 @@ def tela_principal():
     inicio.pack()
 
     hoje = tk.Label(root, text="Revisar hoje:", font=("Arial", 14))
-    hoje.place(x= (largura*0.025), y=((altura)*0.125))
+    hoje.place(x= (largura*0.025), y=((altura+200)*0.125))
 
-    medida_base = desenhar_revisoes_hoje(root) + 200
-    desenhar_revisar_depois(root, medida_base)
+    medida_base = desenhar_revisoes(root, 800, 1) + 200
+
+    revisar_depois = tk.Label(root, text="Revisar depois:", font=('Arial', 14))
+    revisar_depois.place(x = (largura*0.025), y=((200+medida_base)*0.125))
+
+    medida_base2 = desenhar_revisoes(root, medida_base + 200, 2)
+
+    revisoes_atrasadas = tk.Label(root, text="Revisões atrasadas:", font=('Arial', 14))
+    revisoes_atrasadas.place(x = (largura*0.025), y=((medida_base2+200)*0.125))
+
+    desenhar_revisoes(root, medida_base2 + 200, 3)
 
     root.mainloop()
 
-def desenhar_revisar_depois(root, medida_base):
-    cursor.execute("SELECT s_nome, t_id FROM subtopico WHERE data_revisar > (?)", (date.today(), ))
+def desenhar_revisoes(root, medida_base, escolha):
+    global widgets_revisoes
 
-    revisar_depois = tk.Label(root, text="Revisar depois:", font=('Arial', 14))
-    revisar_depois.place(x = (largura*0.025), y=(medida_base*0.125))
+    if escolha == 1:
+        cursor.execute("SELECT s_nome, t_id, s_id FROM subtopico WHERE data_revisar = (?)", (date.today(), ))
+    elif escolha == 2:
+        cursor.execute("SELECT s_nome, t_id, s_id FROM subtopico WHERE data_revisar > (?)", (date.today(), ))
+    elif escolha == 3:
+        cursor.execute("SELECT s_nome, t_id, s_id FROM subtopico WHERE data_revisar < (?)", (date.today(), ))
 
     subtopicos = cursor.fetchall()
     topicos = []
     assuntos = []
     materias = []
+    checkin = []
 
     for x in range (len(subtopicos)):
         cursor.execute("SELECT t_nome, a_id FROM topicos WHERE t_id = (?)", (subtopicos[x][1], ))
@@ -46,6 +61,9 @@ def desenhar_revisar_depois(root, medida_base):
     for x in range (len(assuntos)):
         cursor.execute("SELECT m_nome FROM materias WHERE m_id = (?)", (assuntos[x][0][1], ))
         materias.append(cursor.fetchall())
+
+    for _ in range(len(subtopicos)):
+        checkin.append(tk.IntVar())
 
     espaço = 400
     indice = 0
@@ -55,68 +73,25 @@ def desenhar_revisar_depois(root, medida_base):
     while True:
         if subtopicos != None and indice < len(subtopicos):
             revisao3 = tk.Label(root, text=materias[indice][0], font=("Arial", 10))
-            revisao3.place(x= ((largura + 400)*0.025), y=((medida_base + espaço)*0.125))
-
             revisao2 = tk.Label(root, text=assuntos[indice][0][0], font=("Arial", 10))
-            revisao2.place(x= ((largura + 5900)*0.025), y=((medida_base + espaço)*0.125))
-            
             revisao1 = tk.Label(root, text=topicos[indice][0][0], font=("Arial", 10))
-            revisao1.place(x= ((largura + 11400)*0.025), y=((medida_base + espaço)*0.125))
-
             revisao = tk.Label(root, text=subtopicos[indice][0], font=("Arial", 10))
+            check = tk.Checkbutton(root, text='', variable=checkin[indice], command=lambda s_id=subtopicos[indice][2]: [modulo_update.revisar(cursor, s_id), bancoDados.commit(), desenhar_revisoes(root, medida_base, escolha)])
+
             revisao.place(x= ((largura + 16900)*0.025), y=((medida_base + espaço)*0.125))
+            revisao1.place(x= ((largura + 11400)*0.025), y=((medida_base + espaço)*0.125))
+            revisao2.place(x= ((largura + 5900)*0.025), y=((medida_base + espaço)*0.125))
+            revisao3.place(x= ((largura + 400)*0.025), y=((medida_base + espaço)*0.125))
+            check.place(x= ((largura + 25000)*0.025), y=((medida_base + espaço)*0.125))
 
             espaço += 200
             indice += 1
         else:
             break;
-
-def desenhar_revisoes_hoje(root):
-    
-    cursor.execute("SELECT s_nome, t_id FROM subtopico WHERE data_revisar = (?)", (date.today(), ))
-    
-    subtopicos = cursor.fetchall()
-    topicos = []
-    assuntos = []
-    materias = []
-
-    for x in range (len(subtopicos)):
-        cursor.execute("SELECT t_nome, a_id FROM topicos WHERE t_id = (?)", (subtopicos[x][1], ))
-        topicos.append(cursor.fetchall())
-
-    for x in range (len(topicos)):
-        cursor.execute("SELECT a_nome, m_id FROM assuntos WHERE a_id = (?)", (topicos[x][0][1], ))
-        assuntos.append(cursor.fetchall())
-
-    for x in range (len(assuntos)):
-        cursor.execute("SELECT m_nome FROM materias WHERE m_id = (?)", (assuntos[x][0][1], ))
-        materias.append(cursor.fetchall())
-
-    espaço = 400
-    indice = 0
-
-    desenhar_nomes_colunas(root, largura, altura)
-
-    while True:
-        if subtopicos != None and indice < len(subtopicos):
-            revisao3 = tk.Label(root, text=materias[indice][0], font=("Arial", 10))
-            revisao3.place(x= ((largura + 400)*0.025), y=((altura + espaço)*0.125))
-
-            revisao2 = tk.Label(root, text=assuntos[indice][0][0], font=("Arial", 10))
-            revisao2.place(x= ((largura + 5900)*0.025), y=((altura + espaço)*0.125))
-            
-            revisao1 = tk.Label(root, text=topicos[indice][0][0], font=("Arial", 10))
-            revisao1.place(x= ((largura + 11400)*0.025), y=((altura + espaço)*0.125))
-
-            revisao = tk.Label(root, text=subtopicos[indice][0], font=("Arial", 10))
-            revisao.place(x= ((largura + 16900)*0.025), y=((altura + espaço)*0.125))
-
-            espaço += 200
-            indice += 1
-        else:
-            break;
-
-    return espaço + altura
+    if escolha == 1:
+        return espaço + altura
+    elif escolha == 2:
+        return medida_base + espaço
 
 def desenhar_nomes_colunas(root, largura, altura):
     materias_coluna = tk.Label(root, text="Materias", font=('Arial', 12))
