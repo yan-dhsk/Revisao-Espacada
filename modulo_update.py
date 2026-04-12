e01aef6803106ca910db90b63112eddaa842c5b4
Yan Neves <yanvianaaa@gmail.com>
2026-04-12 19:37:41 -0300

    Correção de erros de conversão
    - A query do SQL retornava data como string causando erros na função 'revisar' ao realizar operações

--- modulo_update.py
+++ modulo_update.py
@@ -70,12 +70,12 @@ def revisar(cursor, s_id):
     cursor.execute("SELECT ultima_revisao, data_estudo, numero_revisoes, data_revisar FROM subtopico WHERE s_id = (?)", (s_id, ))
     datas0 = cursor.fetchone()
     if datas0[2] == 0:
-        data_revisar = datas0[0] + timedelta(days=1)
+        data_revisar = date.fromisoformat(datas0[0]) + timedelta(days=1)
     elif datas0[2] == 1:
-        data_revisar = datas0[0] + timedelta(days=6)
+        data_revisar = date.fromisoformat(datas0[0]) + timedelta(days=6)
     else:
-        intervalo = int(((datas0[0] - datas0[1]).days)*2.5)
-        data_revisar = datas0[0] + timedelta(days=intervalo)
+        intervalo = int(((date.fromisoformat(datas0[0]) - date.fromisoformat(datas0[1])).days)*2.5)
+        data_revisar = date.fromisoformat(datas0[0]) + timedelta(days=intervalo)
     numero_revisoes = datas0[2] + 1
     cursor.execute("UPDATE subtopico SET ultima_revisao = (?), numero_revisoes = (?), data_revisar = (?) WHERE s_id = (?)", (date.today(), numero_revisoes, data_revisar, s_id))
     return True
