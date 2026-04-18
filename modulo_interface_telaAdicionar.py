6f106597a9cd6bfdbe53654515a3f76fe20bc5d1
Yan Neves <yanvianaaa@gmail.com>
2026-04-18 20:56:30 -0300

    Integração da tela principal com outras interfaces
    - Adição de botões que ao clicar chamam as outras interfaces - adicionar, editar, remover.
    - Atualização entre interfaces, apertar um botão em uma janela altera a outra
    - Chamado diretamente e somente pela main

--- modulo_interface_telaAdicionar.py
+++ modulo_interface_telaAdicionar.py
@@ -3,29 +3,28 @@ import sqlite3
 from datetime import date
 import modulo_add
 
-bancoDados = sqlite3.connect("Revisões.db")
-cursor = bancoDados.cursor()
-
 largura = 250
 altura = 340
 
-def tela_adicionar():
-    root = tk.Tk()
+def tela_adicionar(cursor, bancoDados, destruir, root_principal):
+    root = tk.Toplevel()
     root.title("Adicionar")
     root.geometry(f"{largura}x{altura}")
     root.resizable(False, False)
 
     dados_revisao = desenhar_tela_adicionar(root)
-    desenhar_botao(root, dados_revisao)
+    desenhar_botao(cursor, bancoDados, root, dados_revisao, destruir, root_principal)
 
-    root.mainloop()
 
-def desenhar_botao(root, dados_revisao):
-    adicionar = tk.Button(root, text="Adicionar", fg="white", bg="blue", width=20, height=2, command=lambda : verificar(root, cursor, dados_revisao))
-    tk.Label().grid(column=0, row=11)
+def desenhar_botao(cursor, bancoDados, root, dados_revisao, destruir, root_principal):
+    adicionar = tk.Button(root, text="Adicionar", fg="white", bg="blue", width=20, height=2, command=lambda : verificar(root, cursor, dados_revisao, bancoDados, destruir, root_principal))
+    tk.Label(root).grid(column=0, row=11)
     adicionar.grid(column=0, row=12)
 
-def verificar(root, cursor, dados_revisao):
+def verificar(root, cursor, dados_revisao, bancoDados, destruir, root_principal):
+    modulo_add.adicionar_materia(cursor, dados_revisao[0].get())
+    modulo_add.adicionar_assunto(cursor, dados_revisao[0].get(), dados_revisao[1].get())
+    modulo_add.adicionar_topico(cursor, dados_revisao[1].get(), dados_revisao[2].get())
     verificador_repeticao = modulo_add.adicionar_subtopico(cursor, dados_revisao[2].get(), dados_revisao[3].get())
     
     if verificador_repeticao == True:
@@ -35,6 +34,11 @@ def verificar(root, cursor, dados_revisao):
             temp[0].destroy()
         sucesso.grid(column=0, row=13)
         bancoDados.commit()
+
+        from modulo_interface_telaPrincipal import limpar_tela, desenhar_tela
+        limpar_tela(destruir)
+        desenhar_tela(root_principal, cursor, bancoDados)
+
     else:
         falha = tk.Label(root, text="Erro ao adicionar item!", fg="red", font=("Arial", 14))
         if root.grid_slaves(row=13, column=0):
@@ -49,7 +53,7 @@ def desenhar_tela_adicionar(root):
     digitar_materia = tk.Entry(root)
     digitar_materia.grid(column=0, row=1, padx=(4, 0), sticky="nsew")
 
-    tk.Label().grid(column=0, row=2)
+    tk.Label(root).grid(column=0, row=2)
 
     assunto = tk.Label(root, text="Digite o nome do assunto:", font=("Arial", 14))
     assunto.grid(column = 0, row= 3, sticky="w")
@@ -57,7 +61,7 @@ def desenhar_tela_adicionar(root):
     digitar_assunto = tk.Entry(root )
     digitar_assunto.grid(column=0, row=4, padx=(4, 0), sticky="nsew")
 
-    tk.Label().grid(column=0, row=5)
+    tk.Label(root).grid(column=0, row=5)
 
     topico = tk.Label(root, text="Digite o nome do tópico:", font=("Arial", 14))
     topico.grid(column = 0, row= 6, sticky="w")
@@ -65,7 +69,7 @@ def desenhar_tela_adicionar(root):
     digitar_topico = tk.Entry(root )
     digitar_topico.grid(column=0, row=7, padx=(4, 0), sticky="nsew")
 
-    tk.Label().grid(column=0, row=8)
+    tk.Label(root).grid(column=0, row=8)
 
     subtopico = tk.Label(root, text="Digite o nome do subtópico:", font=("Arial", 14))
     subtopico.grid(column = 0, row= 9, sticky="w")
@@ -74,5 +78,3 @@ def desenhar_tela_adicionar(root):
     digitar_subtopico.grid(column=0, row=10, padx=(4, 0), sticky="nsew")
 
     return [digitar_materia, digitar_assunto, digitar_topico, digitar_subtopico]
-
-tela_adicionar()
