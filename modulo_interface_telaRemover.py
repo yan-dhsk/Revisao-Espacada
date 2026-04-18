355ca14705757b2c48f1790c2664045d79f78ed7
Yan Neves <yanvianaaa@gmail.com>
2026-04-18 20:58:15 -0300

    Integração e correção de erros
    - Corrigido erro de janela secundaria agindo como principal
    - A root foi inicializada agora como tk.Toplevel() para evitar problemas
    - Integração da interface com a tela principal, apenas podendo ser aberta por ela

--- modulo_interface_telaRemover.py
+++ modulo_interface_telaRemover.py
@@ -3,25 +3,21 @@ import sqlite3
 from datetime import date
 import modulo_delete
 
-bancoDados = sqlite3.connect("Revisões.db")
-cursor = bancoDados.cursor()
-
 largura = 500
 altura = 480
 
 destruir = []
 
-def tela_deletar():
-    root = tk.Tk()
+def tela_deletar(cursor, bancoDados, destruir2, root_principal):
+    root = tk.Toplevel()
     root.title("Deletar")
     root.geometry(f'{largura}x{altura}')
     root.resizable(False, False)
 
-    desenhar_tela_principal(root, 0)
+    desenhar_tela_principal(root, 0, cursor, bancoDados, destruir2, root_principal)
 
-    root.mainloop()
 
-def desenhar_tela_principal(root, situacao):
+def desenhar_tela_principal(root, situacao, cursor, bancoDados, destruir2, root_principal):
     global destruir
 
     frame = tk.Frame(root)
@@ -37,24 +33,24 @@ def desenhar_tela_principal(root, situacao):
     pergunta.pack()
     destruir.append(pergunta)
 
-    materia = tk.Button(frame, text="Matéria", font=("Arial", 18), bg="blue", fg="white", width=10, height=2, command=lambda x=1: desenhar_lista(root, x))
+    materia = tk.Button(frame, text="Matéria", font=("Arial", 18), bg="blue", fg="white", width=10, height=2, command=lambda x=1: desenhar_lista(root, x, cursor, bancoDados, destruir2, root_principal))
     materia.pack(pady=(10, 0))
     destruir.append(materia)
 
-    assunto = tk.Button(frame, text="Assunto", font=("Arial", 18), bg="blue", fg="white", width=10, height=2, command=lambda x=2: desenhar_lista(root, x))
+    assunto = tk.Button(frame, text="Assunto", font=("Arial", 18), bg="blue", fg="white", width=10, height=2, command=lambda x=2: desenhar_lista(root, x, cursor, bancoDados, destruir2, root_principal))
     assunto.pack(pady=(10, 0))
     destruir.append(assunto)
 
-    tópico = tk.Button(frame, text="Tópico", font=("Arial", 18), bg="blue", fg="white", width=10, height=2, command=lambda x=3: desenhar_lista(root, x))
+    tópico = tk.Button(frame, text="Tópico", font=("Arial", 18), bg="blue", fg="white", width=10, height=2, command=lambda x=3: desenhar_lista(root, x, cursor, bancoDados, destruir2, root_principal))
     tópico.pack(pady=(10, 0))
     destruir.append(tópico)
 
-    subtópico = tk.Button(frame, text="Subtópico", font=("Arial", 18), bg="blue", fg="white", width=10, height=2, command=lambda x=4: desenhar_lista(root, x))
+    subtópico = tk.Button(frame, text="Subtópico", font=("Arial", 18), bg="blue", fg="white", width=10, height=2, command=lambda x=4: desenhar_lista(root, x, cursor, bancoDados, destruir2, root_principal))
     subtópico.pack(pady=(10, 0))
     destruir.append(subtópico)
 
-def desenhar_lista(root, escolha):
-    global cursor, destruir
+def desenhar_lista(root, escolha, cursor, bancoDados, destruir2, root_principal):
+    global destruir
 
     if escolha == 1:
         cursor.execute("SELECT m_nome, m_id FROM materias")
@@ -76,10 +72,12 @@ def desenhar_lista(root, escolha):
     frame2.pack()
 
     for x in range (len(todas_revisoes)):
-        revisao = tk.Button(frame2, text=todas_revisoes[x][0], command=lambda : chamada_deletar(root, escolha, todas_revisoes[x][1]))
+        revisao = tk.Button(frame2, text=todas_revisoes[x][0], command=lambda : chamada_deletar(root, escolha, todas_revisoes[x][1], cursor, bancoDados, destruir2, root_principal))
         revisao.grid(column=0, row=x)
 
-def chamada_deletar(root, escolha, id):
+def chamada_deletar(root, escolha, id, cursor, bancoDados, destruir2, root_principal):
+    from modulo_interface_telaPrincipal import limpar_tela, desenhar_tela
+    
     if escolha == 1:
         modulo_delete.deletar_materia(cursor, id)
     elif escolha == 2:
@@ -93,6 +91,8 @@ def chamada_deletar(root, escolha, id):
     for x in range (len(destruir)):
         destruir[x].destroy()
     destruir.clear()
-    desenhar_tela_principal(root, 1)
 
-tela_deletar()
+    limpar_tela(destruir2)
+    desenhar_tela(root_principal, cursor, bancoDados)
+
+    desenhar_tela_principal(root, 1, cursor, bancoDados, destruir2, root_principal)
